373e6fddaaa3ce1585ea0b03941988e5288b61a3
Dermot Brennan <dermot.brennan@gmail.com>
2016-05-07 11:17:01 +0100

    Improved validations for user and add user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, except: :create
 
   def show
     render json: current_user
@@ -9,6 +9,15 @@ class UsersController < ApplicationController
     @user = User.find(user_id)
     logger.debug user_params.inspect
     if @user.update_attributes(user_params)
+      render json: @user
+    else
+      render json: @user, status: 422, adapter: :json_api, serializer: ActiveModel::Serializer::ErrorSerializer
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
       render json: @user
     else
       render json: @user, status: 422, adapter: :json_api, serializer: ActiveModel::Serializer::ErrorSerializer
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :lists
+
+  validates :username, :name, presence: true
+  validates :username, uniqueness: true
 end
